d5fe0ad8ae33be2697b189128c0fa884add1faf4
Gino Ingreso <geingreso@shakeys.biz>
2023-10-10 16:42:30 +0800

    Created a generic repository for TEST database

---
+++ Repository/Implementations/TestRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Data;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implementations
+{
+    public class TestRepository<T> : IRepository<T> where T : class
+    {
+        private readonly TestContext _context;
+        private DbSet<T> _entity;
+
+        public TestRepository(TestContext testContext)
+        {
+            _context = testContext;
+            _entity = _context.Set<T>();
+        }
+
+        public int Create(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            _entity.Add(entity);
+
+            return _context.SaveChanges();
+        }
+
+        public int Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            _entity.Remove(entity);
+
+            return _context.SaveChanges();
+        }
+
+        public T Read(int id)
+        {
+            return _entity.Find(id);
+        }
+
+        public IList<T> Read(Expression<Func<T, bool>> predicate)
+        {
+            return _entity.Where(predicate).ToList();
+        }
+
+        public int Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            return _context.SaveChanges();
+        }
+    }
+}
---
+++ Repository/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IRepository<T>
+    {
+        int Create(T entity);
+        T Read(int id);
+        IList<T> Read(Expression<Func<T, bool>> predicate);
+        int Update(T entity);
+        int Delete(T entity);
+    }
+}
