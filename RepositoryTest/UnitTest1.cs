94d86ba31d78e3c7cdcf49a4f990717e1b4fa24c
Gino Ingreso <geingreso@shakeys.biz>
2023-10-10 17:09:20 +0800

    Unit test project checkin

---
+++ RepositoryTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Repository;
+using Repository.Implementations;
+using Repository.Interfaces;
+
+namespace RepositoryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //IRepository<Student> _repository;
+
+        [TestInitialize]
+        public void Init()
+        {
+            //_repository = new TestRepository
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
