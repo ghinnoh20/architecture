83beb646fc756488f8bfce34e43dcf991d44f3ad
Gino Ingreso <geingreso@shakeys.biz>
2023-10-10 16:54:23 +0800

    Removed unnecessary USINGs

--- Repository/Implementations/TestRepository.cs
+++ Repository/Implementations/TestRepository.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Repository.Data;
 using Repository.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repository.Implementations
 {
